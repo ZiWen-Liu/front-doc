ca87bc99581c832ba7ac1919c3f25aec1012db92
ZiWen-Liu <ziwen.liu@sayweee.com>
2022-01-07 17:50:29 +0800

    feat: add naming&&type rule

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: {
+      method: "alphabetical",
+      order: [
+        // "Zero",
+        // ["NPM Server", "保存逻辑", "GraphQL_TS自动生成", "Welby"],
+        "规约",
+        ["开发环境", "命名规则", "TypeScript"],
+      ],
+    },
+    showPanel: false,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
